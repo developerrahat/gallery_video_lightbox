30855388a07e68a706dd4204ba5f6eec597b832a
Mohammad Rahat <86203884+developerrahat@users.noreply.github.com>
2021-09-12 13:29:30 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    // Lightbox video play
+    $(".video-play").modalVideo();
+
+
+
+// As A jQuery Plugin -->
+var gallery = $('.gallery a').simpleLightbox({
+
+
+});
+
+});
+
+
+  
+
+
+
+       
+
